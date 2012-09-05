c68d3869fff157616f1be5153b8ee0fdc63e8f9f
David Wright <d.wright.dev@gmail.com>
2012-09-05 09:32:31 -0400

    complete tests

--- src/gocoa/ObjCRuntime_test.go
+++ src/gocoa/ObjCRuntime_test.go
@@ -1,10 +1,27 @@
 package gocoa
 
 import (
+	"fmt"
 //	"runtime"
 	"testing"
 //	"unsafe"
 )
+
+func printlist(name string, methods []Method) {
+	fmt.Println(name)
+	for i:=0; i<len(methods); i++ {
+		fmt.Println("\t", methods[i].Name())
+	}
+}
+
+func printproperties(name string, properties []Property) {
+	fmt.Println(name)
+	for i:=0; i<len(properties); i++ {
+		fmt.Println("\t", properties[i].Name())
+	}
+}
+
+
 	
 /*
 * If it has a test, it's an interface. Most of these are naive tests, when I start discovering
@@ -79,6 +96,15 @@ func Test_Class_Instance(t *testing.T) {
 	}
 }
 
+func Test_Class_Ivar(t *testing.T) {
+	object := ClassForName("NSObject").Instance("alloc")
+	ivar := object.Method("isa")
+	if ivar == 0 {
+		t.Log("ivar", ivar)
+		t.Fail()
+	}
+}
+
 func Test_Class_Method(t *testing.T) {
 	class := ClassForName("NSObject")
 	method := class.Method("alloc")
@@ -89,48 +115,72 @@ func Test_Class_Method(t *testing.T) {
 }
 
 func Test_Class_Property(t *testing.T) {
-	t.Log("where's a good system class with predefined properties?")
-	t.Fail()
-/*	class := ClassForName("NSDate")
-	class.ListProperties()
-	property := class.Property("propname")	
-	if property == 0 {
+	sel := "terminationHandler"
+	nst := ClassForName("NSTask")
+	property := nst.Property(sel)
+	
+	if nst == 0 {
+		t.Log("nst", nst)
 		t.Fail()
-	} else {
-		t.Log("property", property.Name())
-	}*/
+	}
+	if nst.Name() != "NSTask" {
+		t.Log("nst", nst.Name())
+		t.Fail()
+	}
+	
+	if property == 0 {
+		t.Log("property", property)
+		t.Fail()
+	}
+
 }
 
-func Test_Class_InstanceVariables(t *testing.T) {
-	t.Log("unimplemented")
-	t.Fail()
+func Test_Class_Ivars(t *testing.T) {
+	class := ClassForName("NSObject")
+	ivars := class.Ivars()
+	if len(ivars) < 1 {
+		t.Log("ivars", ivars)
+		t.Fail()
+	}
 }
 
 func Test_Class_Methods(t *testing.T) {
-	t.Log("unimplemented")
-	t.Fail()
+	class := ClassForName("NSObject")
+	methods := class.Methods()
+	if len(methods) < 1 {
+		t.Log("methods", methods)
+		t.Fail()
+	}
 }
 
-func Test_Class_Properties(t *testing.T) {
-	t.Log("unimplemented")
-	t.Fail()
+func Test_Class_Properties(t *testing.T) {	
+	class := ClassForName("NSTask")
+	properties := class.Properties()
+	if len(properties) < 1 {
+		t.Log("properties", properties)
+		t.Fail()
+	}
 }
 
 
 func Test_Class_Subclass(t *testing.T) {
 	subclass := ClassForName("NSObject").Subclass("NSSubclass")
 	if subclass == 0 {
+		t.Log("subclass", subclass)
 		t.Fail()
 	}
 	if subclass.Name() != "NSSubclass" {
+		t.Log("subclass", subclass.Name())
 		t.Fail()
 	}
 	
 	instance := subclass.Instance("alloc")
 	if instance == 0 {
+		t.Log("instance", instance)
 		t.Fail()
 	}
 	if instance.Class().Name() != "NSSubclass" {
+		t.Log("instance", instance.Class().Name())
 		t.Fail()
 	}
 }
@@ -200,8 +250,6 @@ func Test_Class_Register(t *testing.T) {
 	}
 	
 }
-
-
 
 func Test_Object_Method(t *testing.T) {
 	object := ClassForName("NSObject").Instance("alloc")
@@ -312,8 +360,33 @@ func Test_Object_Call(t *testing.T) {
 } 
 
 func Test_Object_CallSuper(t *testing.T) {
-	t.Log("unimplemented")
-	t.Fail()
+	
+	bundle := ClassForName("NSBundle").Instance("alloc").Call("initWithPath:", NSString("."))
+	nso := ClassForName("NSObject").Instance("alloc").Call("init")
+	
+	if bundle == 0 {
+		t.Log("bundle", bundle)
+		t.Fail() 
+	}
+	
+ 	nso2 := bundle.CallSuper("init")
+	
+	if nso2 == 0 {
+		t.Log("nso2", nso2)
+		t.Fail() 
+	}
+	
+	// XXX either I'm misunderstanding how this is supposed to work or it's broken
+	if nso.Class().Name() != nso2.Class().Name() {
+		t.Log("nso", nso.Class().Name(), "nso2", nso2.Class().Name())
+		t.Fail() 
+	}
+	
+	if len(nso.Class().Methods()) != len(nso2.Class().Methods()) {
+		t.Log("nso", len(nso.Class().Methods()), "nso2", len(nso2.Class().Methods()))
+		t.Fail() 
+	}
+	
 }
 
 
@@ -330,39 +403,116 @@ func Test_Selector_Name(t *testing.T) {
 
 
 
-
 func Test_Method_ArgumentCount(t *testing.T) {
-	t.Log("unimplemented, wants class.Methods")
-	t.Fail()
+	class := ClassForName("NSObject")
+	method := class.Method("replacementObjectForPortCoder:")
+	if method == 0 {
+		t.Log("method", method)
+		t.Fail()
+	}
+	if method.ArgumentCount() != 3 {
+		t.Log("method", method.Name(), method.ArgumentCount(), "arguments")
+		t.Fail()
+	}
+	
 }
 
 func Test_Method_ArgumentType(t *testing.T) {
-	t.Log("unimplemented, wants class.Methods")
-	t.Fail()
+	class := ClassForName("NSObject")
+	method := class.Method("replacementObjectForPortCoder:")
+	if method == 0 {
+		t.Log("method", method)
+		t.Fail()
+	}
+	if method.ArgumentType(3) != "@" {
+		t.Log("method", method.Name(), "arg", 1, method.ArgumentType(3), "type")
+		t.Fail()
+	}
+	
 }
 
 func Test_Method_Name(t *testing.T) {
-	t.Log("unimplemented, wants class.Methods")
-	t.Fail()
+	sel := "replacementObjectForPortCoder:"
+	class := ClassForName("NSObject")
+	method := class.Method(sel)
+	if method == 0 {
+		t.Log("method", method)
+		t.Fail()
+	}
+	if method.Name() != sel {
+		t.Log("method", method.Name())
+		t.Fail()
+	}
 }
 
 
-
 func Test_Ivar_Name(t *testing.T) {
-	t.Log("unimplemented, wants class.Ivars")
-	t.Fail()
+	sel := "isa"
+	class := ClassForName("NSObject")
+	ivar := class.Ivar(sel)
+	if ivar == 0 || ivar.Name() != sel {
+		t.Log("ivar", ivar)
+		t.Fail()
+	}
 }
 
 
 
 func Test_Property_Name(t *testing.T) {
-	t.Log("unimplemented, wants class.Properties")
-	t.Fail()
+	sel := "terminationHandler"
+	nst := ClassForName("NSTask")
+	property := nst.Property(sel)
+	
+	if nst == 0 {
+		t.Log("nst", nst)
+		t.Fail()
+	}
+	if nst.Name() != "NSTask" {
+		t.Log("nst", nst.Name())
+		t.Fail()
+	}
+	
+	if property == 0 {
+		t.Log("property", property)
+		t.Fail()
+	}
+	if property.Name() != sel {
+		t.Log("property", property.Name())
+		t.Fail()
+	}
+	
 }
 
 func Test_Property_Attributes(t *testing.T) {
-	t.Log("unimplemented, wants class.Properties")
-	t.Fail()
+	sel := "terminationHandler"
+	nst := ClassForName("NSTask")
+	
+	
+	if nst == 0 {
+		t.Log("nst", nst)
+		t.Fail()
+	}
+	if nst.Name() != "NSTask" {
+		t.Log("nst", nst.Name())
+		t.Fail()
+	}
+	
+	property := nst.Property(sel)
+	
+	if property == 0 {
+		t.Log("property", property)
+		t.Fail()
+	}
+	if property.Name() != sel {
+		t.Log("property", property.Name())
+		t.Fail()
+	}
+	
+	attributes := property.Attributes()
+	if len(attributes) == 0 {
+		t.Log("attributes", attributes)
+		t.Fail()
+	}
 }
 
 
